0707f1dee03048bfe961f31305819ca62d59d7fe
srimedha-kotni <24331a05f2@mvgrce.edu.in>
2026-02-22 09:54:59 +0530

    Add files via upload

---
+++ dictfunctions.py
@@ -0,0 +1,8 @@
+dct={1:'medha',2:'kyathi',3:'harika',4:'saranya'}
+print("keys:",dct.keys())
+print("values:",dct.values())
+print("items:",dct.items())
+print(dct.pop(3))
+print("after pop operation:",dct)
+del dct[4]
+print("after delete operation:",dct)
